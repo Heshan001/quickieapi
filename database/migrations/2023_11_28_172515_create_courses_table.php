5997d4531de82bc30349777183f935addb51d0ba
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2023-11-30 12:03:02 +0530

    create courses migration
    create courses migration

---
+++ database/migrations/2023_11_28_172515_create_courses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('courseName');
+            $table->string('courseOverview');
+            $table->string('courseContent');
+            $table->string('coursePayment');
+            $table->string('minimumResult');
+            $table->string('zCore');
+            $table->string('image');
+            $table->unsignedBigInteger('institute_id');
+            $table->foreign('institute_id')->references('id')->on('institutes')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
