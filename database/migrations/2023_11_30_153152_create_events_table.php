b0689faf06c266c4547c6fbcf7cd7024e8f78b51
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2023-12-01 04:04:54 +0530

    create event migration

---
+++ database/migrations/2023_11_30_153152_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('eventName');
+            $table->unsignedBigInteger('institute_id');
+            $table->string('eventDescription');
+            $table->string('addImage');
+            $table->foreign('institute_id')->references('id')->on('institutes')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
