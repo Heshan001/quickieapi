8eba9bd623e5d0a8b38f17d8d8f31a6bbe5da6ed
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2023-12-03 23:16:53 +0530

    C:\xampp\htdocs\Quickie Project\quickieapi\app\Models\Event.php
    C:\xampp\htdocs\Quickie Project\quickieapi\app\Models\Event.php

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,7 +9,7 @@ class Event extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'courseName', 'courseOverview', 'courseContent', 'minimumResult', 'subjectStream', 'zCore', 'image', 'institute_id',
+        'eventName', 'institute_id', 'eventDescription', 'Image',
     ];
 
 
