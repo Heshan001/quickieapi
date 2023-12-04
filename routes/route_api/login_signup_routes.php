b6a3f42efdd478e614e3898661394293e20337dd
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2023-12-04 21:55:21 +0530

    login signup api
    // apis

--- routes/route_api/login_signup_routes.php
+++ routes/route_api/login_signup_routes.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\AuthController;
 
 
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/signup', [AuthController::class, 'signup']);
+// Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/signup', [AuthController::class, 'signup']);
