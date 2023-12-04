829f0c0bd01d4bfec587779e0ba87fe21d89bd93
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2023-12-04 21:54:54 +0530

    update api.php
    routes login signup get to out of the folder

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/signup', [AuthController::class, 'signup']);
 
-
-Route::group([], base_path('routes/route_api/login_signup_routes.php'));
+//Route::group([], base_path('routes/route_api/login_signup_routes.php'));
 Route::group([], base_path('routes/route_api/course_routes.php'));
 Route::group([], base_path('routes/route_api/event_routes.php'));
 
