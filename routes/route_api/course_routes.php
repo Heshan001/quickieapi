0083ada4850e7dd5f4300678a1af625331ce6209
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2023-12-05 19:39:05 +0530

    commet line

--- routes/route_api/course_routes.php
+++ routes/route_api/course_routes.php
@@ -5,9 +5,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+//Route::get('/student/get_allCourseList/{limit?}/{page?}/{query?}', [CourseCrudController::class, 'AllInstituteCourseList']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/course/store', [CourseCrudController::class, 'store']);
     Route::delete('/course/delete/{course_id}', [CourseCrudController::class, 'delete']);
     Route::get('/course/get_list/{limit?}/{page?}/{query?}', [CourseCrudController::class, 'instituteCourseList']);
+
 });
