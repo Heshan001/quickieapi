fd8cff940fdfdfc3ec8014095f1dc2d701876e6b
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2023-12-06 01:17:34 +0530

    student_routes.php
    update

--- routes/route_api/student_routes.php
+++ routes/route_api/student_routes.php
@@ -1,9 +1,11 @@
 <?php
 use App\Http\Controllers\Api\Event\EventCrudController;
 use App\Http\Controllers\Api\Course\CourseCrudController;
+use App\Http\Controllers\Api\News\NewsCrudController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/student/get_allCourseList/{limit?}/{page?}/{query?}', [CourseCrudController::class, 'AllInstituteCourseList']);
 Route::get('/student/get_allEventList/{limit?}/{page?}/{query?}', [EventCrudController::class, 'AllInstituteEventList']);
+Route::get('/student/get_allNewsList/{limit?}/{page?}/{query?}', [NewsCrudController::class, 'AllNewsList']);
