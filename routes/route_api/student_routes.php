18f705c0c8e8500fe7459c7ad4bf0b6ace9ce846
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2023-12-05 19:39:35 +0530

    update student route
    update student route

--- routes/route_api/student_routes.php
+++ routes/route_api/student_routes.php
@@ -1,8 +1,9 @@
 <?php
-
+use App\Http\Controllers\Api\Event\EventCrudController;
 use App\Http\Controllers\Api\Course\CourseCrudController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/student/get_allCourseList/{limit?}/{page?}/{query?}', [CourseCrudController::class, 'AllInstituteCourseList']);
+Route::get('/student/get_allEventList/{limit?}/{page?}/{query?}', [EventCrudController::class, 'AllInstituteEventList']);
