7eb5a7c1222bbb06d0cea4ef5c7c0b7baa6d875b
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2023-12-07 16:18:00 +0530

    update comment routes
    update comment routes

--- routes/route_api/comment_routes.php
+++ routes/route_api/comment_routes.php
@@ -8,5 +8,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/comment/store', [CommentCrudController::class, 'store']);
-    Route::delete('/student/delete/{event_id}', [CommentCrudController::class, 'delete']);
+    
 });
